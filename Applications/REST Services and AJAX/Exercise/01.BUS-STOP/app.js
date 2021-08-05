dd5cb9ab5d0fc8d5bb55dee6ed38e483b60152b7
Simona Ilcheva <s.ilcheva@gmail.com>
2021-08-05 11:46:06 +0300

    update

--- Applications/REST Services and AJAX/Exercise/01.BUS-STOP/app.js
+++ Applications/REST Services and AJAX/Exercise/01.BUS-STOP/app.js
@@ -1,3 +1,24 @@
 function getInfo() {
-    console.log("TODO...");
+    let validBuses = ["1287", "1308", "1327", "2334"];
+    let stopId = document.getElementById("stopId");
+    let stopInfo = document.getElementById("stopName");
+    let busesList = document.getElementById("buses");
+
+    if (!validBuses.includes(stopId.value)) {
+        stopInfo.textContent = "Error";
+        busesList.textContent = "";
+        return;
+    }
+    const url = `https://my-first-app-js-default-rtdb.europe-west1.firebasedatabase.app/businfo/${stopId.value}.json`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            stopInfo.textContent = data.name;
+            Object.keys(data.buses).forEach((key) => {
+                let li = document.createElement("li");
+                li.textContent = `Bus ${key} arrives in ${data.buses[key]}`;
+                busesList.appendChild(li);
+            });
+            stopId.value = "";
+        });
 }
