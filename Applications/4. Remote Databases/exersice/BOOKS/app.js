f8a9634729a9543516750422262e5bf461ec0b1f
Simona Ilcheva <s.ilcheva@gmail.com>
2021-10-20 16:43:09 +0300

    update

--- Applications/4. Remote Databases/exersice/BOOKS/app.js
+++ Applications/4. Remote Databases/exersice/BOOKS/app.js
@@ -6,16 +6,19 @@ const htmlSelectors = {
     'createIsbnInput': () => document.getElementById('create-isbn'),
     'booksContainer': () => document.querySelector('table > tbody'),
     'errorContainer': () => document.getElementById('error-notification'),
+    'editForm': () => document.getElementById('edit-form'),
     'editBtn': () => document.querySelector('#edit-form > button'),
     'editTitleInput': () => document.getElementById('edit-title'),
     'editAuthorInput': () => document.getElementById('edit-author'),
     'editIsbnInput': () => document.getElementById('edit-isbn'),
+    'deleteBtn': () => document.getElementById
 }
 htmlSelectors['loadBooks']()
     .addEventListener('click', fetchAllBooks)
 htmlSelectors['createBtn']()
     .addEventListener('click', createBook)
-
+htmlSelectors['editBtn']()
+    .addEventListener('click', editBook)
 
 function fetchAllBooks() {
     fetch('https://books-exersice-default-rtdb.europe-west1.firebasedatabase.app/Books/.json')
@@ -41,7 +44,7 @@ function renderBooks(booksData) {
                 createDOMElement('td', isbn, {}, {}),
                 createDOMElement('td', '', {}, {},
                     createDOMElement('button', 'Edit', { 'data-key': bookId }, { click: loadBookById }),
-                    createDOMElement('button', 'Delete', {}, {})));
+                    createDOMElement('button', 'Delete', { 'data-key': bookId }, { click: deleteBook })));
 
             booksContainer.appendChild(tableRow)
         })
@@ -86,13 +89,68 @@ function createBook(e) {
 
 function loadBookById() {
     const id = this.getAttribute('data-key')
-    console.log(id);
+    fetch(`https://books-exersice-default-rtdb.europe-west1.firebasedatabase.app/Books/${id}.json`)
+        .then(res => res.json())
+        .then(({ title, author, isbn }) => {
+            htmlSelectors['editTitleInput']().value = title;
+            htmlSelectors['editAuthorInput']().value = author;
+            htmlSelectors['editIsbnInput']().value = isbn;
+            htmlSelectors['editForm']().style.display = 'block';
+            htmlSelectors['editBtn']().setAttribute('data-key', id)
+
+        })
+        .catch(handleError)
 }
 
+function editBook(e) {
+    e.preventDefault();
+    const titleInput = htmlSelectors['editTitleInput']();
+    const authorInput = htmlSelectors['editAuthorInput']();
+    const isbnInput = htmlSelectors['editIsbnInput']();
+
+    if (titleInput.value !== '' && authorInput.value !== '' && isbnInput.value !== '') {
+        const id = this.getAttribute('data-key');
+        const initObj = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title: titleInput.value, author: authorInput.value, isbn: isbnInput.value })
+        }
+        htmlSelectors['editForm']().style.display = 'none';
+        fetch(`https://books-exersice-default-rtdb.europe-west1.firebasedatabase.app/Books/${id}/.json`, initObj)
+            .then(fetchAllBooks)
+            .catch(handleError)
+
+    } else {
+        const error = { message: '' };
+        if (titleInput.value === '') {
+            error.message += 'What\'s the title? '
+        }
+        if (authorInput.value === '') {
+            error.message += 'What\'s the author? '
+        }
+        if (isbnInput.value === '') {
+            error.message += 'What\'s the ISBN? '
+        }
+        handleError(error);
+    }
+
+}
+function deleteBook() {
+
+    const id = this.getAttribute('data-key')
+    const initObj = {
+        method: 'DELETE'
+    }
+    fetch(`https://books-exersice-default-rtdb.europe-west1.firebasedatabase.app/Books/${id}/.json`, initObj)
+        .then(fetchAllBooks)
+        .catch(handleError)
+}
 function handleError(err) {
     const errorContainer = htmlSelectors['errorContainer']();
     errorContainer.style.display = 'block';
-    errorContainer.textContent = err.message
+    errorContainer.textContent = err.message;
 
     setTimeout(() => {
         errorContainer.style.display = 'none';
