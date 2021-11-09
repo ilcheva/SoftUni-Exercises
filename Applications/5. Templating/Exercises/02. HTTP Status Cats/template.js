565b287a17528def9c160994d1b43e37f675a53a
Simona Ilcheva <s.ilcheva@gmail.com>
2021-11-09 16:15:30 +0200

    update on templates

--- Applications/5. Templating/Exercises/02. HTTP Status Cats/template.js
+++ Applications/5. Templating/Exercises/02. HTTP Status Cats/template.js
@@ -1,8 +1,35 @@
-(() => {
-     renderCatTemplate();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+const elements = {
+    allCats: () => document.getElementById('allCats')
+}
+Promise.all([
+    getTemplate('./template.hbs'),
+    getTemplate('./cat.hbs')
+])
+    .then(([templateSource, catSrc]) => {
+        Handlebars.registerPartial('cat', catSrc)
+        let template = Handlebars.compile(templateSource);
+        let resultHtml = template({ cats });
+        elements.allCats().innerHTML = resultHtml;
+        attachEventListener()
+    })
+    .catch((e) => console.error(e));
+
+function getTemplate(templateLocation) {
+    return fetch(templateLocation).then((r) => r.text())
+}
+function attachEventListener() {
+    elements.allCats().addEventListener('click', (e) => {
+        const { target } = e;
+        if (target.nodeName === 'BUTTON' && target.className === 'showBtn') {
+            let divStatus = target.parentNode.querySelector('div.status')
+            if (divStatus.style.display === 'none') {
+                divStatus.style.display = 'block';
+                e.target.textContent = 'Hide status code';
+            } else {
+                divStatus.style.display = 'none';
+                e.target.textContent = 'Show status code';
+            }
+        }
+    })
+}
