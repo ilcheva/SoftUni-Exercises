193c6c55a0bd0273ae3e4428d91e9f709500ee87
Simona Ilcheva <s.ilcheva@gmail.com>
2021-11-14 02:22:40 +0200

    templates update

--- Applications/5. Templating/Exercises/03. Popular Monkeys/monkeysTemplate.js
+++ Applications/5. Templating/Exercises/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,29 @@
-$(() => {
-    // TODO
-})
+import monkeys from './monkeys.js';
+
+
+const elements = {
+    allMonkeys: () => document.querySelector('div.monkeys')
+}
+fetch('./monkey.hbs')
+    .then((r) => r.text())
+    .then((monkeysTemplateSrc) => {
+        const monkeyTemplate = Handlebars.compile(monkeysTemplateSrc)
+        let resultHtml = monkeyTemplate({ monkeys })
+        elements.allMonkeys().innerHTML = resultHtml;
+        attachEventListener();
+    })
+    .catch((e) => console.error(e))
+function attachEventListener() {
+    elements.allMonkeys().addEventListener('click', (e) => {
+        const { target } = e;
+
+        if (target.nodeName === 'BUTTON') {
+            let infoParagraph = target.parentNode.querySelector('div.monkey > p')
+            if (infoParagraph.style.display === 'none') {
+                infoParagraph.style.display = 'block';
+            } else {
+                infoParagraph.style.display = 'none';
+            }
+        }
+    })
+}
