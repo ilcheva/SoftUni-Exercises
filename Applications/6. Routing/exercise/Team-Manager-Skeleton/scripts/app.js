5bf4b4bc608633201913602ea19f6fd11eb565fa
Simona Ilcheva <s.ilcheva@gmail.com>
2021-11-17 17:42:16 +0200

    updated with firebase and webpack

--- Applications/6. Routing/exercise/Team-Manager-Skeleton/scripts/app.js
+++ Applications/6. Routing/exercise/Team-Manager-Skeleton/scripts/app.js
@@ -1,18 +1,83 @@
 
+import firebaseApp from "./firebase.js";
+import {
+    getFirestore,
+    addDoc,
+    collection
+} from "@firebase/firestore";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
+} from "@firebase/auth";
+
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+
 
 const router = Sammy('#main', function () {
 
     this.use('Handlebars', 'hbs')
 
     this.get('/home', function (context) {
+        const userInfo = localStorage.getItem('userInfo');
+
+        if (userInfo) {
+            const { uid, email } = JSON.parse(userInfo)
+            context.loggedIn = true;
+            context.email = email;
+        }
         loadPartials(context).then(function () {
             this.partial('../templates/home/home.hbs')
         })
     });
-
     this.get('/about', function (context) {
+        const userInfo = localStorage.getItem('userInfo');
+
+        if (userInfo) {
+            const { uid, email } = JSON.parse(userInfo)
+            context.loggedIn = true;
+            context.email = email;
+        }
         loadPartials(context).then(function () {
             this.partial('../templates/about/about.hbs')
+        })
+    });
+
+    this.get('/catalog', function (context) {
+        const userInfo = localStorage.getItem('userInfo');
+
+        if (userInfo) {
+            const { uid, email } = JSON.parse(userInfo)
+            context.loggedIn = true;
+            context.email = email;
+            context.hasNoTeam = true;
+        }
+        this.loadPartials({
+            'header': '../templates/common/header.hbs',
+            'footer': '../templates/common/footer.hbs',
+            'team': '../templates/catalog/team.hbs'
+        }).then(function () {
+            this.partial('../templates/catalog/teamCatalog.hbs')
+        })
+    });
+    this.get('/create', function (context) {
+        const userInfo = localStorage.getItem('userInfo');
+
+        if (userInfo) {
+            const { uid, email } = JSON.parse(userInfo)
+            context.loggedIn = true;
+            context.email = email;
+
+        }
+        this.loadPartials({
+            'header': '../templates/common/header.hbs',
+            'footer': '../templates/common/footer.hbs',
+            'createForm': '../templates/create/createForm.hbs'
+        }).then(function () {
+            this.partial('../templates/create/createPage.hbs');
         })
     });
 
@@ -25,7 +90,14 @@ const router = Sammy('#main', function () {
             this.partial('../templates/login/loginPage.hbs')
         })
     });
-
+    this.get('/logout', function (context) {
+        signOut(auth)
+            .then((res) => {
+                localStorage.removeItem('userInfo')
+                context.redirect('/home')
+            })
+            .catch((e) => console.log(e))
+    })
     this.get('/register', function () {
         this.loadPartials({
             'header': '../templates/common/header.hbs',
@@ -36,17 +108,36 @@ const router = Sammy('#main', function () {
         })
     });
 
+
+
     //POST
 
     this.post('/register', function (context) {
         const { email, password, repeatPassword } = context.params;
-
+        if (password !== repeatPassword) {
+            let err = document.querySelector('#errorBox')
+            err.textContent = 'Passwords doesn\'t match!'
+            err.style.display = 'block';
+            return;
+        }
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                console.log(userCredential);
+                // console.log(userCredential.user);
+                this.redirect('/login')
             })
+            .catch((error) => console.log(error))
 
     })
+    this.post('/login', function (context) {
+        const { email, password } = context.params;
+        signInWithEmailAndPassword(auth, email, password)
+            .then(({ user: { uid, email } }) => {
+                localStorage.setItem('userInfo', JSON.stringify({ uid, email }));
+                context.redirect('/home')
+            })
+            .catch((e) => console.log(e))
+    });
+    // post for create form page 
 
 });
 (() => {
@@ -59,3 +150,5 @@ function loadPartials(context) {
         'footer': '../templates/common/footer.hbs'
     })
 }
+
+/// TO FINISH 
--- Applications/6. Routing/exercise/Team-Manager-Skeleton/scripts/firebase.js
+++ Applications/6. Routing/exercise/Team-Manager-Skeleton/scripts/firebase.js
@@ -1,37 +1,25 @@
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged, getRedirectResult } from 'firebase/auth';
+import { initializeApp } from "@firebase/app";
+// import { getAuth, onAuthStateChanged, getRedirectResult } from 'firebase/auth';
 
 
 // TODO: Add SDKs for Firebase products that you want to use
-
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyBCZ_pfb9KrnJ1e_7iYi_XvvFlQbh5bBcI",
-
     authDomain: "my-first-app-js.firebaseapp.com",
-
     databaseURL: "https://my-first-app-js-default-rtdb.europe-west1.firebasedatabase.app",
-
     projectId: "my-first-app-js",
-
     storageBucket: "my-first-app-js.appspot.com",
-
     messagingSenderId: "813259151791",
-
     appId: "1:813259151791:web:291167bd3c5759c426775a"
-
 };
-
 
 // Initialize Firebase
 
 const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp)
+export default firebaseApp;
 
