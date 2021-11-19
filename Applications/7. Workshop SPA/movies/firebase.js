5d9db020fc14d418929a64a3c6da4f6945bd047c
Simona Ilcheva <s.ilcheva@gmail.com>
2021-11-19 14:44:21 +0200

    movies workshop spa

---
+++ Applications/7. Workshop SPA/movies/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDjowGNcWnSTDK6pmqMZknJJ1CkyxTi74w",
+
+  authDomain: "movies-14b9f.firebaseapp.com",
+
+  projectId: "movies-14b9f",
+
+  storageBucket: "movies-14b9f.appspot.com",
+
+  messagingSenderId: "912382124991",
+
+  appId: "1:912382124991:web:ee3707dc2481051c15770a"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
